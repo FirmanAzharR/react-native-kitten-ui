949d236a43aa96ea5aeec0f5c0eb2d05a5764b4d
FirmanAzharR <firmanazharr@gmail.com>
2023-03-16 15:00:15 +0700

    test ui

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Router from "./src/router";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
+// import theme from "./src/themes/theme";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={{ ...eva.light }}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { Button, Divider, Layout, TopNavigation } from "@ui-kitten/components";
 import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
-  const navigation = useNavigation();
-
   return (
-    <>
-      <Text>Login Screen</Text>
-    </>
+    <SafeAreaView style={styles.container}>
+      <TopNavigation alignment="center" />
+      <Divider />
+      <Layout style={styles.layout}>
+        <Button onPress={() => console.log("test")}>OPEN DETAILS</Button>
+      </Layout>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  layout: {
+    flex: 1,
+  },
+});
 
 export default LoginScreen;
